7b6f3b48d260a17f52b4f8b9851fb9a1daeaf5e2
devdoubleh2 <dev.ismoiljon@gmail.com>
2026-04-07 17:12:06 +0500

    update

--- test_prop_api.py
+++
@@ -1,9 +0,0 @@
-from apps.property.views import PropertyListCreateView
-from django.test import RequestFactory
-import traceback
-request = RequestFactory().get('/api/property/properties/')
-try:
-    response = PropertyListCreateView.as_view()(request)
-    print("STATUS_CODE:", response.status_code)
-except Exception:
-    traceback.print_exc()
